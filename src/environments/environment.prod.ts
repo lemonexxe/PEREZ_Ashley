06a403d07e79d51f7aad26e880fd986a4a9fd257
genro214 <genro214@gmail.com>
2024-05-26 02:13:24 +0800

    act10

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyDcFRPZiJSGqH9MsHmQFd6jlQf-gg8UKNc",
-    authDomain: "act10-d72e4.firebaseapp.com",
-    projectId: "act10-d72e4",
-    storageBucket: "act10-d72e4.appspot.com",
-    messagingSenderId: "255807331221",
-    appId: "1:255807331221:web:1f01355218b028e26a749c"
+    apiKey: "AIzaSyDjbm8etokTDMQ8G5VEKf_LvNjJy1J44_0",
+    authDomain: "act10-5472c.firebaseapp.com",
+    projectId: "act10-5472c",
+    storageBucket: "act10-5472c.appspot.com",
+    messagingSenderId: "449665391518",
+    appId: "1:449665391518:web:2f7e089afd73b7d520148f"
   }
 };
